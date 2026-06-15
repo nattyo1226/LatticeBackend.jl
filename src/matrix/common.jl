cdf61aef48af106abe7bffde08718d54a4374fd0
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2026-06-15 20:53:11 +0900

    ♻️ Refactor build_matrix functions to accept Vector types for ids and matrices, enhancing flexibility and consistency

--- src/matrix/common.jl
+++ src/matrix/common.jl
@@ -1,15 +1,19 @@
 function build_matrix(
     ::Type{T},
     num_sites::Int,
-    ids_site::NTuple{K,Int},
-    matrices::NTuple{K,<:AbstractMatrix},
-) where {T<:Number,K}
+    ids::Vector{Int},
+    matrices::Vector{<:AbstractMatrix},
+) where {T<:Number}
+    if length(ids) != length(matrices)
+        throw(ArgumentError("Length of ids and matrices must be the same"))
+    end
+
     matrix_result = one(T)
-    identity_matrix = build_matrix(T, Identity)
+    identity_matrix = build_matrix(T, Identity())
 
     for id in 1:num_sites
-        if id in ids_site
-            matrix = matrices[findfirst(x -> x == id, ids_site)]
+        if id in ids
+            matrix = matrices[findfirst(x -> x == id, ids)]
             matrix_result = kron(matrix_result, matrix)
         else
             matrix_result = kron(matrix_result, identity_matrix)
@@ -22,8 +26,8 @@ end
 function build_matrix(
     ::Type{T},
     num_sites::Int,
-    id_site::Int,
+    id::Int,
     matrix::AbstractMatrix,
 ) where T<:Number
-    return build_matrix(T, num_sites, (id_site,), (matrix,))
+    return build_matrix(T, num_sites, [id], [matrix])
 end
